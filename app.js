8f0c624231449813d8c3502ee7a075be7b45ff20
Anthony Angelo <angelo_anthony@bah.com>
2019-11-08 22:11:28 -0500

    Update

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Swagger Route
+var swaggerUi = require('swagger-ui-express');
+
+// Express Server
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Swagger Route
+swaggerDocument = require('./api/swagger.json');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
